4cf8377042bc2a1067f6cfbf838e4e5868203355
wjhueston <wjhueston@gmail.com>
2020-11-23 19:58:16 -0600

    Updated for Class 11 Lab

--- array-hunt.js
+++ array-hunt.js
@@ -93,44 +93,96 @@ $(document).ready(function () {
         Find the first and last string in the array.
         Output them to td#firstLast
          */
-
+        var firstElement = myArray[0]
+        var lastElement = myArray[myArray.length-1]
+        $("td#firstLast").text(firstElement + ", " + lastElement)
 
         /*
         Find the first string that contains an 'n'.
         Output it to td#firstEnn
          */
-
-
+        var i
+        var o = 0
+        var firstN
+        for(i=0; i<myArray.length; i++){
+            if(myArray[i].includes("n") && o===0){
+                firstN = myArray[i]
+                o++
+            }
+        }
+        $("td#firstEnn").text(firstN)
         /*
         Find all of the strings with less than 6 characters.
         Output them to td#lessThanSix
          */
-
-
+        var e
+        var sixChars = []
+        for(e=0; e<myArray.length; e++){
+            if(myArray[e].length < 6){
+                sixChars.push(myArray[e])
+            }
+        }
+        $("td#lessThanSix").text(sixChars.toString())
         /*
         Find the longest string in the array.
         Output it to td#longName
          */
-
-
+        var a
+        var longest = ""
+        for(a=0; a<myArray.length; a++){
+            if(myArray[a].length > longest.length){
+                longest = myArray[a]
+            }
+        }
+        $("td#longName").text(longest)
         /*
         Find all of the strings that do not contain the letter 's'.
         Output them to td#noEss
          */
-
+        var noSAllowed = []
+        var u
+        for(u=0; u<myArray.length; u++){
+            if(!myArray[u].includes("s")){
+                noSAllowed.push(myArray[u])
+            }
+        }
+        $("td#noEss").text(noSAllowed.toString())
 
         /*
         Output all of the strings, but with all of their vowels
         in uppercase, to td#upperVowels
          */
-
-
+        //So completing this objective was easy. Looking at the result was hard.
+        var t
+        var s
+        var vowelsUpper = []
+        var stringUpper = ""
+        for(t=0; t<myArray.length; t++){
+            for(s=0; s<myArray[t].length; s++){
+                if(myArray[t].charAt(s) === "a" || myArray[t].charAt(s) === "e" || myArray[t].charAt(s) === "i" || myArray[t].charAt(s) === "o" || myArray[t].charAt(s) === "u"){
+                    stringUpper = stringUpper + myArray[t].charAt(s).toUpperCase()
+                }
+                else{
+                    stringUpper = stringUpper + myArray[t].charAt(s)
+                }
+            }
+            vowelsUpper.push(stringUpper)
+            stringUpper = ""
+        }
+        $("td#upperVowels").text(vowelsUpper)
         /*
         Output all of the strings in reverse order and separated by
         ' - ' to td#reverseDash
          */
-
-
+        var r
+        var reverseOrder = []
+        reverseOrder.toString = function() {
+            return this.join(' - ');
+        };
+        for(r=0; r<myArray.length; r++){
+            reverseOrder.push(myArray[myArray.length - r-1])
+        }
+        $("td#reverseDash").text(reverseOrder.toString())
     }
 
 });
